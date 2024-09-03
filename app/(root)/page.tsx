80801f19a3e6320a33a2f531c3324c74c8defb95
ammrshmbng <ammarmare01@gmail.com>
2024-09-03 17:40:27 +0700

    add chartjs component with data dummy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,32 @@ import React from "react";
 
 const Home = () => {
   const loggedIn = {firstname:"Ammar Sihombing"}
+  const acount = [
+    {
+      name: "Bank A", currentBalance: 500.00,
+      id: "12345",
+      availableBalance: 450.00,
+      officialName: "Bank Alpha",
+      mask: "1234",
+      institutionId: "inst_001",
+      type: "checking",
+      subtype: "personal",
+      appwriteItemId: "item_001",
+      shareableId: "share_001"
+    },
+    {
+      name: "Bank B", currentBalance: 750.35,
+      id: "67890",
+      availableBalance: 700.00,
+      officialName: "Bank Beta",
+      mask: "5678",
+      institutionId: "inst_002",
+      type: "savings",
+      subtype: "business",
+      appwriteItemId: "item_002",
+      shareableId: "share_002"
+    }
+  ]
   return (
     <section className="home">
       <div className="home-content">
@@ -16,7 +42,7 @@ const Home = () => {
 
          />
          <TotalBalanceBox
-            accounts={[]}
+            accounts={acount}
             totalBanks={1}
             totalCurrentBalance={1250.35}
          />
