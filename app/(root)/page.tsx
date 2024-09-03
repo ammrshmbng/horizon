af658b4e439f6288086abaf9faa039c04a5cf2c9
ammrshmbng <ammarmare01@gmail.com>
2024-09-03 17:22:42 +0700

    add countup animated component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
@@ -13,6 +14,11 @@ const Home = () => {
             user={loggedIn?.firstname || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
 
+         />
+         <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
          />
         </header>
       </div>
